82b73fe9266ec489c10378cd7d7cd1ef22082e4d
balachandar <govindalapudibalachandar@gmail.com>
2021-08-25 15:49:57 +0530

    Added EmpWageForMonth file is added

--- src/com/bridgelabz/EmpWgeBuilder.java
+++ src/com/bridgelabz/EmpWgeBuilder.java
@@ -4,7 +4,7 @@ public class EmpWgeBuilder {
 	public static final int IS_FULL_TIME = 1;
 	public static final int IS_PART_TIME = 2;
 	public static final int RATE_PER_HOUR = 20;
-	
+	public static final int NUM_OF_WORKING_DAYS=20;
 
 	public static void main(String[] args) {
 		 
@@ -12,7 +12,10 @@ public class EmpWgeBuilder {
 	
 		int emphrs = 0;
 		 int empwage=0;
-		
+		int totalempwage = 0;
+		 
+		 for(int day =0 ;day < NUM_OF_WORKING_DAYS; day++)
+		 {
 		int empcheck = (int) Math.floor(Math.random () *10) %3;
 		switch ( empcheck ) 
 		{
@@ -26,8 +29,11 @@ public class EmpWgeBuilder {
 			emphrs=0;
 		}
 		empwage = emphrs * RATE_PER_HOUR;
-		
+		totalempwage += empwage;
 		System.out.println("EmpWage :"+empwage);
+		 }
+		 System.out.println("Total Emp Wage :"+totalempwage);
+		
 		
 		
 
