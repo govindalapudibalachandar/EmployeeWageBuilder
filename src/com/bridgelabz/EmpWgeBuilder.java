e146359fc815c5c63e7ebf36c368b9d37c141cef
balachandar <govindalapudibalachandar@gmail.com>
2021-08-25 12:34:49 +0530

    Added UC1_EmpAttendce file is added

--- src/com/bridgelabz/EmpWgeBuilder.java
+++ src/com/bridgelabz/EmpWgeBuilder.java
@@ -5,6 +5,18 @@ public class EmpWgeBuilder {
 	public static void main(String[] args) {
 		 
 		System.out.println("Welcome To Employee Wage Computation");
+		
+		int IS_FULL_TIME =1;
+		
+		Double empcheck = Math.floor(Math.random () *10) %2;
+		if (empcheck==IS_FULL_TIME)
+		{
+			System.out.println("Employee Is Present");
+		}
+		else
+		{
+			System.out.println("Employee Is Absent");
+		}
 
 	}
 
