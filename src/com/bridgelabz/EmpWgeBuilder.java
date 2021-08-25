767dfdc9e10bd022941dfbd637705609b8efa87e
balachandar <govindalapudibalachandar@gmail.com>
2021-08-25 13:06:02 +0530

    Added:EmpDailyWage file is added

--- src/com/bridgelabz/EmpWgeBuilder.java
+++ src/com/bridgelabz/EmpWgeBuilder.java
@@ -6,17 +6,20 @@ public class EmpWgeBuilder {
 		 
 		System.out.println("Welcome To Employee Wage Computation");
 		
-		int IS_FULL_TIME =1;
+		int IS_FULL_TIME = 1;
+		int RATE_PER_HOUR = 20;
+		
+		int emphrs = 0;
+		 int empwage=0;
 		
 		Double empcheck = Math.floor(Math.random () *10) %2;
 		if (empcheck==IS_FULL_TIME)
-		{
-			System.out.println("Employee Is Present");
-		}
-		else
-		{
-			System.out.println("Employee Is Absent");
-		}
+			emphrs = 8;
+		empwage = emphrs * RATE_PER_HOUR;
+		
+		System.out.println("EmpWage :"+empwage);
+		
+		
 
 	}
 
