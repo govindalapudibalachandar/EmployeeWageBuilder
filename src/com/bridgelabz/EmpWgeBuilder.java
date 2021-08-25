e94039631edb599de3ef73b77454a19373a448de
balachandar <govindalapudibalachandar@gmail.com>
2021-08-25 13:16:27 +0530

    Added EmpWagePrtTime file is added

--- src/com/bridgelabz/EmpWgeBuilder.java
+++ src/com/bridgelabz/EmpWgeBuilder.java
@@ -7,14 +7,17 @@ public class EmpWgeBuilder {
 		System.out.println("Welcome To Employee Wage Computation");
 		
 		int IS_FULL_TIME = 1;
+		int IS_PART_TIME = 2;
 		int RATE_PER_HOUR = 20;
 		
 		int emphrs = 0;
 		 int empwage=0;
 		
-		Double empcheck = Math.floor(Math.random () *10) %2;
+		Double empcheck = Math.floor(Math.random () *10) %3;
 		if (empcheck==IS_FULL_TIME)
 			emphrs = 8;
+		if (empcheck==IS_PART_TIME)
+			emphrs = 4;
 		empwage = emphrs * RATE_PER_HOUR;
 		
 		System.out.println("EmpWage :"+empwage);
