1b1458e97fe30470e6110a6780f5837d36342485
balachandar <govindalapudibalachandar@gmail.com>
2021-08-25 13:47:41 +0530

    Added EmpWageSwitchCase file is added

--- src/com/bridgelabz/EmpWgeBuilder.java
+++ src/com/bridgelabz/EmpWgeBuilder.java
@@ -1,23 +1,30 @@
 package com.bridgelabz;
 
 public class EmpWgeBuilder {
+	public static final int IS_FULL_TIME = 1;
+	public static final int IS_PART_TIME = 2;
+	public static final int RATE_PER_HOUR = 20;
+	
 
 	public static void main(String[] args) {
 		 
 		System.out.println("Welcome To Employee Wage Computation");
-		
-		int IS_FULL_TIME = 1;
-		int IS_PART_TIME = 2;
-		int RATE_PER_HOUR = 20;
-		
+	
 		int emphrs = 0;
 		 int empwage=0;
 		
-		Double empcheck = Math.floor(Math.random () *10) %3;
-		if (empcheck==IS_FULL_TIME)
-			emphrs = 8;
-		if (empcheck==IS_PART_TIME)
+		int empcheck = (int) Math.floor(Math.random () *10) %3;
+		switch ( empcheck ) 
+		{
+		case IS_PART_TIME:
 			emphrs = 4;
+			break;
+		case IS_FULL_TIME:
+			emphrs = 8;
+			break;
+		default:
+			emphrs=0;
+		}
 		empwage = emphrs * RATE_PER_HOUR;
 		
 		System.out.println("EmpWage :"+empwage);
